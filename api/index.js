9032c7efb41d8b6de5862b0245bd862c3ecaf10f
Pedro Antlia <pedro@antlia.com.br>
2026-05-27 13:44:59 -0300

    fix: step 3 - JSON content-type + JSON body

--- api/index.js
+++ api/index.js
@@ -6,8 +6,12 @@ import app from '../server/index.js';
 export default function handler(req, res) {
   if (req.url === '/api/debug') {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('OK ' + req.method + ' ' + req.url);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+      method: req.method,
+      url: req.url,
+      bodyType: typeof req.body,
+    }));
     return;
   }
 
